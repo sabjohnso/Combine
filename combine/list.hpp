ab75a6f660c935e52e6223e6ddd06b6f4f2736ae
Samuel B. Johnson <sabjohnso@yahoo.com>
2015-05-21 21:18:51 -0500

    Added list header file.

--- combine/list.hpp
+++ combine/list.hpp
@@ -2,6 +2,11 @@
 #define LIST_HPP_INCLUDED_1568740812345049774 1
 
 
+// Standard header files
+
+#include <cstddef>
+
+
 // Combine header files
 
 #include <combine/nil.hpp>
@@ -20,10 +25,16 @@ namespace Combine
     using type = Nil;
   };
 
-  template< typename X, typename ... Xs >
-  struct List< X, Xs ... >
+  template< typename X >
+  struct List< X >
   {
-    using type = Cons< X, List< Xs ... > >;
+    using type = Cons< X, Nil >;
+  };
+
+  template< typename X0, typename X1, typename ... Xs >
+  struct List< X0, X1, Xs ... >
+  {
+    using type = Cons< X0, typename List< X1, Xs ... >::type >;
   };
 
   
@@ -40,9 +51,53 @@ namespace Combine
   };
 
   template< typename X, typename Y >
-  struct Listp
+  struct Listp< Cons< X, Y > >
   {
     constexpr static bool value = Listp< Y >::value;
+  };
+
+  template< typename >
+  struct Length;
+
+  template< typename X >
+  struct Length
+  {
+    COMBINE_STATIC_ASSERT_TRUE( Listp< X >::value );
+    constexpr static size_t value = 1+Length< typename Cdr< X >::type >::value;
+  };
+
+  template<>
+  struct Length< Nil >
+  {
+    constexpr static size_t value = 0;
+  };
+
+
+  template< typename X >
+  struct Reverse
+  {
+
+    COMBINE_STATIC_ASSERT_TRUE( Listp< X >::value );
+    
+    template< typename Y, typename Accum >
+    struct Aux
+    {
+      COMBINE_STATIC_ASSERT_TRUE( Listp< Y >::value );
+      COMBINE_STATIC_ASSERT_TRUE( Listp< Accum >::value );
+
+      using type = typename Aux< typename Cdr< Y >::type, Cons< typename Car< Y >::type, Accum > >::type;
+      
+    };
+
+    template< typename Accum >
+    struct Aux< Nil, Accum >
+    {
+      COMBINE_STATIC_ASSERT_TRUE( Listp< Accum >::value );
+      using type = Accum;
+    };
+
+    using type = typename Aux< X, Nil >::type;
+
   };
 
 } // end of namespace Combine
--- test/list_test.cpp
+++ test/list_test.cpp
@@ -1,5 +1,50 @@
+/** @file test/list_test.cpp
+    
+    @author Samuel B. Johnson <sabjohnso@yahoo.com>
+
+ */
+
+
+// Combine header files
+
+#include <combine/macro_tools.hpp>
+#include <combine/nil.hpp>
+#include <combine/cons.hpp>
+#include <combine/list.hpp>
+
 int
 main( int argc, char** argv )
 {
-  return 1;
+  using Combine::Nil;
+  using Combine::List;
+  using Combine::Listp;
+  using Combine::Consp;
+  using Combine::Length;
+  using Combine::Reverse;
+  using Combine::Car;
+  using Combine::Cdr;
+  using Integer_types = typename List< short, int, long long >::type;
+  
+  
+  COMBINE_STATIC_ASSERT_TRUE( Listp< Nil >::value );
+  COMBINE_STATIC_ASSERT_TRUE( Consp< Integer_types >::value );
+  COMBINE_STATIC_ASSERT_TRUE( Listp< Integer_types >::value );
+
+  COMBINE_STATIC_ASSERT_EQUAL( 0, Length< Nil >::value );
+  COMBINE_STATIC_ASSERT_EQUAL( 3, ARG( Length< Integer_types >::value ));
+  COMBINE_STATIC_ASSERT_SAME_TYPE( short, Car< Integer_types >:: type );
+
+  using Integer_types_reversed = typename Reverse< Integer_types >::type;
+
+  COMBINE_STATIC_ASSERT_TRUE( Listp< Integer_types_reversed >::value );
+  COMBINE_STATIC_ASSERT_EQUAL( 3, ARG( Length< Integer_types_reversed >::value ));
+  COMBINE_STATIC_ASSERT_SAME_TYPE( long long, Car< Integer_types_reversed >::type );
+  
+  
+			       
+  
+
+  return 0;
+
+  
 }
